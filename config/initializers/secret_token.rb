9381c0e11fcb21d6da54eaca15cad35511a12903
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-09-19 13:03:10 +0200

    rails new cerberus -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cerberus::Application.config.secret_token = '184e970d34fff65f5cff6fbbf4706a104f15a636adc8ee55c9a9970906e4a9305a63d8ea0d79b6479fde90be0259dedc3cb7a134a61c3ab935cf5fa34001b9b5'
