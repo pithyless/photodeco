042a960cb2e73e899f1e478bfeb1fe267afa24c0
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-09-21 12:35:58 +0200

    active_admin for Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 
-User.create! :first_name => 'Norbert', :last_name => 'Wojtowicz', :email => 'user@example.com', :password => 'secret', :password_confirmation => 'secret'
+User.create! :first_name => 'Norbert', :last_name => 'Wojtowicz', :email => 'a@b.com', :password => 'secret', :password_confirmation => 'secret'
