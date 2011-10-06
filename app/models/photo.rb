017a69751193e6ea0daa69d02a22781254a9892d
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-10-06 16:08:52 +0200

    parse basic photo metadata

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -21,12 +21,111 @@ class Photo < ActiveRecord::Base
                   :copyright, :notes, :taken_at, :digitized_at,
                   :image, :provider_id
 
+  before_validation :hardcode_provider
   before_validation :parse_image_metadata
 
+  def hardcode_provider
+    # Temporary - always set to Forum
+    self[:provider_id] = Provider.find_by_slug!('agencja-forum')
+  end
+
+  class MetaPhoto
+    IGNORED_TAGS = ['Iptc.Application2.0x00bb', 'Iptc.Application2.0x00f0',
+                    'Iptc.Application2.0x00e7']
+    # DATE_TAGS = ['Iptc.Application2.DateCreated', 'Exif.Image.DateTime']
+
+    def initialize
+      @meta = Hash.new { |h, k| h[k] = [] }
+    end
+
+    def []=(key, value)
+      key   = ::ActiveSupport::Inflector.transliterate(key)
+      return nil if IGNORED_TAGS.include? key
+      value = ::ActiveSupport::Inflector.transliterate(value)
+      @meta[key] << value
+    end
+
+    def [](key)
+      value = @meta[key]
+      case value.length
+      when 0; nil
+      when 1; value[0]
+      else;   value
+      end
+    end
+
+    def iptc(key)
+      case key
+      when /\w+\.\w+\.\w+/
+        self[key]
+      when /\w+\.\w+/
+        self['Iptc.' + key]
+      else
+        self['Iptc.Application2.' + key]
+      end
+    end
+
+    def exif(key)
+      case key
+      when /\w+\.\w+\.\w+/
+        self[key]
+      when /\w+\.\w+/
+        self['Exif.' + key]
+      else
+        self['Exif.Image.' + key]
+      end
+    end
+
+    def each &blk
+      @meta.keys.each do |k|
+        blk.call([k, self[k]])
+      end
+    end
+
+    def digitized_at
+      require 'chronic'
+      # TODO: 2011:07:22 09:44:53 - meta.exif('DateTime')
+      v = self.iptc('DateCreated') || self.exif('DateTime')
+      Chronic::parse(v)
+    end
+
+    def keywords
+      Array(self.iptc('Keywords')).join(', ')
+    end
+  end
+
   def parse_image_metadata
+    image = Exiv2::ImageFactory.open(self.image.file.path)
+    image.read_metadata
+
+    meta = MetaPhoto.new
+
+    image.iptc_data.each do |key, value|
+      meta[key] = value
+    end
+    image.exif_data.each do |key, value|
+      meta[key] = value
+    end
+
+    self[:title] = meta.iptc('Headline')
+    self[:description] = [meta.iptc('Headline'), meta.iptc('Byline'),
+                          meta.iptc('Caption')].compact.join("\n\n")
+    self[:author] = meta.iptc('Writer')
+    self[:credit] = meta.iptc('Credit')
+    self[:source]
+    self[:copyright] = [meta.iptc('Copyright'),
+                        meta.iptc('SpecialInstructions')].compact.join("\n\n")
+    self[:taken_at]     = meta.digitized_at
+    self[:digitized_at] = meta.digitized_at
+
+    notes = []
+    notes << "Keywords: #{meta.keywords} ||"
+    meta.each { |k,v| notes << "#{k} => #{v}" }
+    self[:notes] = notes.join('\n')
+
     # TODO
-    self[:image_original_filename] = 'todo.jpg'
-    self[:image_original_secure_token] = 's3cr3t2'
-    self[:image_secure_token] = 's3cr3t1'
+    self[:image_original_filename] = self.image.file.original_filename
+    self[:image_original_secure_token] = 'secretOne'
+    self[:image_secure_token] = 'secretTwo'
   end
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,66 @@
 require 'spec_helper'
 
-describe Photo do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Photo, 'Metadata' do
+  describe 'topfoto_0035386.jpg' do
+    before(:all) do
+      @photo = Photo.new
+      @photo.image = File.open(File.join(Rails.root, 'spec/files',
+                                    'topfoto_0035386.jpg'))
+    end
+
+    subject { @photo }
+
+    it { should be_valid }
+    specify { subject.save.should == true }
+
+    its(:image_original_filename) { should == 'topfoto_0035386.jpg' }
+    its(:image_original_secure_token) { should == 'secretOne' }
+    its(:image_secure_token) { should == 'secretTwo' }
+
+    its(:title) { should == 'Victoria Plums' }
+    its(:description) do
+      should == "Victoria Plums\n\n?2000 Credit:Topham Picturepoint\n\nVictoria Plums *** Local Caption *** C00944073"
+    end
+    its(:author) { should == 'Nicky E' }
+    its(:credit) { should == 'TopFoto' }
+    its(:taken_at) { should == DateTime.parse('2000/03/07 11:00:00 UTC +00:00')  }
+    its(:digitized_at) { should == DateTime.parse('2000/03/07 11:00:00 UTC +00:00') }
+    
+    its(:copyright) { should == '' }
+
+    # TODO: its(:source) { should == 'Foo' }
+  end
+
+  describe 'EP_20061105_mgo_g90_395.jpg' do
+    before(:all) do
+      @photo = Photo.new
+      @photo.image = File.open(File.join(Rails.root, 'spec/files',
+                                    'EP_20061105_mgo_g90_395.jpg'))
+    end
+
+    subject { @photo }
+
+    it { should be_valid }
+    specify { subject.save.should == true }
+
+    its(:image_original_filename) { should == 'EP_20061105_mgo_g90_395.jpg' }
+    its(:image_original_secure_token) { should == 'secretOne' }
+    its(:image_secure_token) { should == 'secretTwo' }
+
+    its(:title) { should == '1965 - Faster Pussycat Kill Kill - Movie Set' }
+
+    its(:description) do
+      should == "1965 - Faster Pussycat Kill Kill - Movie Set\n\nEve Productions\n\nRELEASE DATE: 1965. DIRECTOR: Russ Meyer. STUDIO: Eve Productions. PLOT: Three strippers seeking thrills encounter a young couple in the desert. After dispatching the boyfriend, they take the girl hostage and begin scheming on a crippled old man living with his two sons in the desert, reputedly hiding a tidy sum of cash. ..PICTURED: LORI WILLIAMS (as Billie), HAJI (as Rosie), TURA SATANA (as Varla).  Title Faster Pussycat! Kill! Kill!"
+    end
+
+    its(:author) { should == 'Entertainment Pictures' }
+    its(:credit) { should == 'Entertainment Pictures' }
+    its(:taken_at) { should == DateTime.parse('1965/06/01 11:00:00 UTC +00:00')  }
+    its(:digitized_at) { should == DateTime.parse('1965/06/01 11:00:00 UTC +00:00') }
+
+    its(:copyright) { should == "? 1965 by Entertainment Pictures\n\nThis image is NOT available for commercial or promotional use and is being made available for editorial reference usage only." }
+
+    # TODO: its(:source) { should == 'Foo' }
+
+  end
 end
